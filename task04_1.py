dc18991053cc09af889f546e509c0a480eb00b6c
Oleksandr Golubchyk <81289118+GOS81@users.noreply.github.com>
2025-06-18 22:02:16 +0300

    task 1 for hw-04 finished

---
+++ task04_1.py
@@ -0,0 +1,25 @@
+# Завдання 1
+
+# У вас є текстовий файл, який містить інформацію про місячні заробітні плати розробників у вашій компанії. Кожен рядок у файлі містить прізвище розробника та його заробітну плату, які розділені комою без пробілів.
+# Ваше завдання - розробити функцію total_salary(path), яка аналізує цей файл і повертає загальну та середню суму заробітної плати всіх розробників.
+
+import re
+from pathlib import Path
+
+def total_salary(path):
+    with open(path, "r", encoding="utf-8") as file:
+        i = 0
+        total = 0
+        while True:
+            line = file.readline()
+            salary = re.search(r"\d+", line)
+            if not line:
+                break
+            salary = int(salary.group())
+            total = total + salary
+            i += 1
+        average = total // i
+    return total, average
+
+total, average = total_salary("salary.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
