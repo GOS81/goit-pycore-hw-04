7133a61502dce2177b412abcd7c4fceeba04910a
Oleksandr Golubchyk <81289118+GOS81@users.noreply.github.com>
2025-07-03 14:49:53 +0300

    added  decorftor

--- task04_4/parse_input.py
+++ task04_4/parse_input.py
@@ -1,7 +1,8 @@
-from handler_command import add, change, show, all
+from handler_command import add, change, show, all, input_error
 
 contact_dictionary = []
 
+@input_error
 def parse_input(input_command):
     cmd, *arg = input_command.split()
     cmd = cmd.lower()
@@ -21,8 +22,11 @@ def parse_input(input_command):
         add(contact_name, contact_phone, contact_dictionary)
 
     elif cmd == "change":
-        change(contact_name, contact_phone, contact_dictionary)
-
+        if contact_name != None and contact_phone != None:
+            change(contact_name, contact_phone, contact_dictionary)
+        else:
+            raise ValueError ("Give me name and phone please")
+    
     elif cmd == "show":
         show(contact_name, contact_dictionary)
     
@@ -30,6 +34,6 @@ def parse_input(input_command):
         all(contact_dictionary)
 
     else:
-        print("Команда не знайдена! Спробуйте ще раз.")
+        raise ValueError("Command not found! Try one of these: hello, add, change, show, all, exit or close")
 
     return contact_dictionary
